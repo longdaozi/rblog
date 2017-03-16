ff626ec573abe22b6e2d3fba0c35773e5a022abf
longdaozi <trapmiui@gmail.com>
2017-03-16 12:28:05 +0800

    Static Pages

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,4 @@
 class Micropost < ApplicationRecord
-	belongs_to :user
-	validates :content, length: {maximum: 140}
-                        presence: true
+  belongs_to :user
+  validates :content, length: {maximum: 140}                       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :microposts
   resources :users
-  root 'users#index'
+  root 'application#hello'
 end

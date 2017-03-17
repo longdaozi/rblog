c39e586446e098a4252934873338b16b60dbfe76
longdaozi <trapmiui@gmail.com>
2017-03-17 22:30:43 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get 'static_pages/about'
   resources :microposts
   resources :users
-  root 'application#hello'
 end
